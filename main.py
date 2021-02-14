fb2308a2be5a741826ccd024ae8bbf6aa6b525ea
SpeedWagonIT <gartem9999@gmail.com>
2021-02-15 00:31:22 +0300

    alpha version 0.1 Тестирую работу функций, возможность импорта из других файлов, использование псевдонимов, метод canvas, метод create_text

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from tkinter import *
-mainwin = Tk()
-mainwin.mainloop()
+import tkinter as tk
+import _func_module as fm
+root = Tk()
+
+c = Canvas(root, width=1280, height=1024, bg='white')
+c.pack()
+
+c.create_line(20, 20, 20, 180)
+c.create_line(20, 180, 180, 180)
+c.create_line(180, 180, 180, 20)
+c.create_line(180, 20, 20, 20)
+
+c.create_text(80, 80,
+              text="1\n2\n3\n4",
+              justify=CENTER, font="Verdana 15")
+fm._hello()
+root.mainloop()
